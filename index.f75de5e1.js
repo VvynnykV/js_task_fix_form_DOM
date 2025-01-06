8c8974bca223041a5c6d4f09523f0ba70c556e6b
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2025-01-06 12:35:30 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+'use strict';
+const fields = [
+    ...document.querySelectorAll('input')
+];
+fields.forEach((field)=>{
+    const label = document.createElement('label');
+    label.classList.add('field-label');
+    label.setAttribute('for', `${field.id}`);
+    label.innerText = field.name;
+    field.before(label);
+    const placeholderValue = field.name.charAt(0).toUpperCase() + field.name.slice(1);
+    field.setAttribute('placeholder', placeholderValue);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
